29bc813c65b16c1b5ff07319c6fde16712e7e215
William E Little Jr <git@bmo.dev>
2022-02-25 18:55:48 -0600

    Cleanup make.bash

--- website/make.bash
+++ website/make.bash
@@ -2,34 +2,131 @@
 
 shopt -s extglob
 
-# Utils
-readonly cf="\\033[0m"
-readonly red="\\033[0;31m"
-readonly green="\\033[0;32m"
-readonly yellow="\\033[0;33m"
-readonly purple="\\033[0;35m"
+#########
+# Utils #
+#########
 
-function err() {
-    printf -- "[%s][${red}ERROR${cf}]: %s\n" "$(showdate)" "$1"
+# Ansi color code variables
+readonly blue="\e[0;94m"
+readonly cyan="\e[1;96m"
+readonly green="\e[0;92m"
+readonly purple="\e[1;95m"
+readonly red="\e[0;91m"
+readonly yellow="\e[1;93m"
+readonly white="\e[0;97m"
+
+readonly expand_bg="\e[K"
+readonly blue_bg="\e[0;104m${expand_bg}"
+readonly red_bg="\e[0;101m${expand_bg}"
+readonly green_bg="\e[0;102m${expand_bg}"
+
+readonly bold="\e[1m"
+readonly uline="\e[4m"
+readonly reset="\e[0m"
+
+
+_date_format="+%d/%b/%Y %H:%M:%S"
+# success "Finished install"
+#   $1  required    The string to log
+#
+function success () {
+    local _date
+
+    _date="$(command -p date "$_date_format")"
+
+    printf "${green}SUCCESS${reset} [%s] %s\n" "$_date" "${1:-Success}"
 }
 
-function warn() {
-    printf -- "[%s][${yellow}WARNING${cf}]: %s\n" "$(showdate)" "$1"
+# debug "Using download mirror: $mirror"
+#   $1  required    The string to log
+#
+function debug () {
+    [ "${_debug:-FALSE}" == FALSE  ] && return
+
+    local _date
+
+    _date="$(command -p date "$_date_format")"
+    read -r _line _sub _file < <(caller 0)
+
+    printf 'DEBUG [%s] <%s@%s:%s> %s\n' "$_date" "$_sub" "$_file" "$_line" "${1:-Unknown Error}"
 }
 
-function info() {
-    printf -- "[%s][INFO]: %s\n" "$(showdate)" "$1"
+# info "Download completel"
+#   $1  required    The string to log
+#
+function info () {
+    local _date _line _sub _file
+
+    _date="$(command -p date "$_date_format")"
+    read -r _line _sub _file < <(caller 0)
+
+    printf "${blue}INFO${reset} [%s] <%s@%s:%s> %s\n" "$_date" "$_sub" "$_file" "$_line" "${1:-Unknown Error}"
 }
 
-function succ() {
-    printf -- "[%s][${green}SUCCESS${cf}]: %s\n" "$(showdate)" "$1"
+# warn "Outdated version, consider updating"
+#   $1  required    The string to log
+#
+function warn () {
+    local _date _line _sub _file
+
+    _date="$(command -p date "$_date_format")"
+    read -r _line _sub _file < <(caller 0)
+
+    printf "${yellow}WARN${reset} [%s] <%s@%s:%s> %s\n" "$_date" "$_sub" "$_file" "$_line" "${1:-Unknown Error}"
 }
 
-function showdate() {
-    printf '%s' "$(command -p date '+%d/%b/%y %T %z')"
+# error "Cloud not download dependency: $dep"
+#   $1  required    The string to log
+#
+function error () {
+    local _date _line _sub _file
+
+    _date="$(command -p date "$_date_format")"
+    read -r _line _sub _file < <(caller 0)
+
+    >&2 printf "${red}ERROR${reset} [%s] <%s@%s:%s> %s\n" "$_date" "$_sub" "$_file" "$_line" "${1:-Unknown Error}"
+    [ "${_backtrace:-FALSE}" == FALSE ] || print_backtrace 1
 }
 
-# Variables
+# die "Cannot open file: $file" 2
+#   $1  required    The string to log
+#   $2  optional    The exit code (defaults: 1)
+#
+function die () {
+    local _date
+
+    _date="$(command -p date "$_date_format")"
+    >&2 printf "${red}${bold}FATAL${reset} [%s] %s\n" "$_date" "${1:-Unknown Error}"
+
+    print_backtrace 1
+
+    exit "${2:-1}"
+}
+
+# print_backtrace 1
+#   $1  optional    non-negative integer representing stack frame (defaults: 0)
+#
+function print_backtrace () {
+    local _date _frame
+
+    _frame="${1:-0}"
+    _date="$(command -p date "$_date_format")"
+
+    >&2 printf "${red}${bold}BACKTRACE${reset} [%s] " "$_date"
+
+    local _line _sub _file
+    while read -r _line _sub _file < <(caller "$_frame"); do
+        >&2 printf '%s@%s:%s <= ' "$_sub" "$_file" "$_line"
+        ((_frame++))
+    done
+
+    >&2 printf '%s\n' "$SHELL"
+}
+
+#############
+# Variables #
+#############
+
 DIR_CONF='./config/'
 CONF_TTREE="${DIR_CONF}/ttree.cfg"
 DIR_ASSETS='./assets/'
@@ -41,9 +138,13 @@ CSS_SRC='style/retro/css/retro.css'
 CSS_DST="${DIR_DIST}/css/style.css"
 SERVE_PORT='8080'
 
+##############
+# Main Logic #
+##############
+
 # Targets
 function print_help() {
-    cat <<"EOF"
+    cat <<'EOF'
 Automated targets:
     help:       Show this help message
     clean:      Clean up directories
@@ -59,38 +160,53 @@ EOF
     exit 0
 }
 
+function prompt_remove() {
+    local _dir="$1"
+
+    [ -e "$_dir" ] || return 0
+
+    read -r -p "Remove “$_dir” forcibly and recursively? [yn] " yn
+    [ "$yn" = "y" ] \
+        || die "Bailing..."
+
+    command -p rm -frv "$_dir" \
+        || die "Failed to clean “$_dir”"
+
+    return 0
+}
+
 function make_clean() {
-    info 'Cleaning up directories...'
-    if command -p rm -rfv "$DIR_MD" "$DIR_DIST"; then
-        succ 'Finished cleaning'
-        return 0
-    else
-        err 'Failed to clean properly'
-        return 1
-    fi
+    info "Cleaning up directories... “$DIR_MD” “$DIR_DIST” “$DIR_VAR_WWW”"
+
+    prompt_remove "$DIR_MD"
+    prompt_remove "$DIR_DIST"
+    prompt_remove "$DIR_VAR_WWW"
+
+    success 'Finished cleaning'
+    return 0
 }
 
 function make_css() {
     info 'Creating CSS...'
+
     [ -d "${CSS_DST%/*}" ] || mkdir -vp "${CSS_DST%/*}"
     if command -p cp -v "$CSS_SRC" "$CSS_DST"; then
-        succ 'Finished creating CSS files'
+        success 'Finished creating CSS files'
         return 0
-    else
-        err 'Failed to create CSS files properly'
-        return 1
     fi
+
+    die 'Failed to create CSS files properly'
 }
 
 function make_markdown() {
     info "Creating markdown files..."
+
     if command ttree -f "$CONF_TTREE"; then
-        succ 'Finshed creating markdown files'
+        success 'Finshed creating markdown files'
         return 0
-    else
-        err 'Failed to create markdown files properly'
-        return 1
     fi
+
+    die 'Failed to create markdown files properly'
 }
 
 function make_html() {
@@ -108,7 +224,9 @@ function make_html() {
 
             # Pandoc will not auto create output directories
             local output="${file/${DIR_MD}/${DIR_DIST}}"
-            [ -d "${output%/*}" ] || mkdir -vp "${output%/*}"
+            [ -d "${output%/*}" ] \
+                || command -p mkdir -vp "${output%/*}" \
+                || die "Failed to create output directory “${output}”"
 
             command pandoc \
                 --css "$css_url" \
@@ -118,26 +236,29 @@ function make_html() {
                 --output "${output/%.md/.html}" \
                 "$file"
 
-            printf '\n'
         done
 
-    # Clean the webroot first
-    info "Cleaning webroot “$DIR_VAR_WWW"
-    command -p rm -rfv  "$DIR_VAR_WWW"
-
     # Copy assets to dist
-    info "Copping assets"
-    command -p cp -avr "$DIR_ASSETS" "$DIR_DIST"
-    command -p cp -av "${DIR_SRC}/robots.txt" "$DIR_DIST"
+    info "Copying “$DIR_ASSETS” to “$DIR_DIST”"
+    command -p cp -avr "$DIR_ASSETS" "$DIR_DIST" \
+        || die "Failed to copy “$DIR_ASSETS” to “$DIR_DIST”"
+
+    command -p cp -av "${DIR_SRC}/robots.txt" "$DIR_DIST" \
+        || die "Failed to copy “${DIR_SRC}/robots.txt” to “$DIR_DIST”"
+
 
     # Copy dist to webroot
-    info "Copying dist to “$DIR_VAR_WWW”"
-    command -p cp -avr "$DIR_DIST" "$DIR_VAR_WWW"
+    info "Copying “$DIR_DIST” to “$DIR_VAR_WWW”"
+    command -p cp -avr "$DIR_DIST" "$DIR_VAR_WWW" \
+        || die "Failed to “$DIR_DIST” to “$DIR_VAR_WWW”"
 
     # Drop permission
-    info 'Reducing Permissions'
-    command -p find "$DIR_VAR_WWW" -type d -exec chmod -v 0750 '{}' \+
-    command -p find "$DIR_VAR_WWW" -type f -exec chmod -v 0440 '{}' \+
+    info "Setting Permissions in “$DIR_VAR_WWW”"
+    command -p find "$DIR_VAR_WWW" -type d -exec chmod -v 0750 '{}' \+ \
+        || die "Failed to chmod dirs 0750 in “$DIR_VAR_WWW”"
+
+    command -p find "$DIR_VAR_WWW" -type f -exec chmod -v 0440 '{}' \+ \
+        || die "Failed to chmod files 0440 in “$DIR_VAR_WWW”"
 }
 
 function make_resume() {
@@ -147,7 +268,7 @@ function make_resume() {
     date="$(command date '+%F')"
 
     if ! [ -f 'dist/hire.html' ]; then
-        err "Missing source file “dist/hire.html”. Did you run “$0 dist”?"
+        error "Missing source file “dist/hire.html”. Did you run “$0 dist”?"
         return 1
     fi
 
@@ -168,34 +289,22 @@ function make_resume() {
 
 function make_dist() {
     # Cleanup first
-    if ! make_clean; then
-        err 'Bailing...'
-        return 1
-    fi
+    make_clean || die 'Bailing...'
 
     # Create our dist directory
     [ -d "$DIR_DIST" ] || mkdir -vp "$DIR_DIST"
 
     # Create our CSS
-    if ! make_css; then
-        err 'Bailing...'
-        return 1
-    fi
+    make_css || die 'Bailing...'
 
     # Create our markdown files 
     info 'Creating distribution files...'
-    if ! make_markdown; then
-        err 'Bailing...'
-        return 1
-    fi
+    make_markdown || die 'Bailing...'
 
     # Create our html files
-    if ! make_html; then
-        err 'Bailing...'
-        return 1
-    fi
+    make_html || die 'Bailing...'
 
-    succ 'Finished creating distribution files'
+    success 'Finished creating distribution files'
 }
 
 function make_serve() {
@@ -236,7 +345,7 @@ while :; do
             break
             ;;
         +([a-zA-Z0-9-]))
-            err "Unknown target “$1”"
+            error "Unknown target “$1”"
             print_help
             ;;
         *)
